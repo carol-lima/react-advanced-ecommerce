ad1aaf72dc879072c238d8fb569a5711d4c832de
carol-lima <carollima.dev@gmail.com>
2022-02-21 19:16:31 -0300

    Added redux, and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 
 import './index.css';
 import App from './App';
 
+/* Provider: Component class we get from react-redux that once passed the store object
+will be able to give that redux store context to the rest of the application, so we can dispatch 
+actions to that store or we can actually pull values off of the store and into our components. */
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
